853dfc63147ec28bc8a6f8a1d2d553b035855f44
zHElEARN <501088732@qq.com>
2020-02-26 07:49:52 +0800

    修改命令行参数

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,8 +16,26 @@
 
 namespace json = rapidjson;
 
-std::string GetToken()
+enum class Method {
+	DOWNLOAD = 1,
+	UPLOAD = 2,
+	OTHER = 3
+};
+
+bool FileExistsStatus(const char* path)
 {
+	DWORD dwAttribute = GetFileAttributes(path);
+	if (dwAttribute == 0XFFFFFFFF) return false;
+	else return true;
+}
+
+bool DownloadFiles(const char* url, const char* downloadPath)
+{
+	if (URLDownloadToFile(NULL, url, downloadPath, 0, 0) == S_OK && FileExistsStatus(downloadPath)) return true;
+	else return false;
+}
+
+std::string GetToken() {
 	std::string token;
 
 	auto response = cpr::Get(
@@ -28,19 +46,18 @@ std::string GetToken()
 			{"from", "static"}
 		}
 	);
-	dbg("Token状态码", response.status_code);
+	dbg("获得Token 状态码", response.status_code);
 
 	json::Document document;
 	document.Parse(response.text.c_str());
 
 	token = document["data"][0]["token"].Get<const char*>();
-	dbg("Token", token);
+	dbg("获得Token", token);
 
 	return token;
 }
 
-void Upload(const std::string& name)
-{
+void Upload(const std::string& name) {
 	dbg("上传", name);
 
 	std::string token = GetToken();
@@ -54,79 +71,82 @@ void Upload(const std::string& name)
 		}
 	);
 
-	dbg("上传状态码", response.status_code);
+	dbg("上传 状态码", response.status_code);
 	
 	json::Document document;
 	document.Parse(response.text.c_str());
 
-	dbg("哈希", document["hash"].Get<const char*>());
-	dbg("键值", document["key"].Get<const char*>());
+	dbg("上传 哈希", document["hash"].Get<const char*>());
+	dbg("上传 键值", document["key"].Get<const char*>());
 
 	return;
 }
 
-void Download(const std::string& key)
-{
+void Download(const std::string& key) {
 	std::string url = "https://static.codemao.cn/" + key;
-	dbg("正在下载Url", url);
+	std::string name = key;
 
-	HRESULT result = URLDownloadToFile(NULL, url.c_str(), key.c_str(), 0, NULL);
-	if (result == S_OK)
-		dbg("下载成功", result);
+	dbg("下载 Url", url);
+
+	bool result = DownloadFiles(url.c_str(), name.c_str());
+	if (result)
+		dbg("下载 成功", result);
 	else
-		dbg("下载失败", result);
+		dbg("下载 失败", result);
 
 	return;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	argparse::ArgumentParser program("CodemaoMaterial");
 
 	{
-		program.add_argument("-m", "--method")
+		program.add_argument("method")
 			.help("选择上传/下载方法")
 			.required()
-			.action([](const std::string& value) { return value; })
-			.default_value("download");
+			.action([](const std::string& value) {
+			if (value == "upload")
+				return Method::UPLOAD;
+			else if (value == "download")
+				return Method::DOWNLOAD;
+			else
+				return Method::OTHER;
+			});
 
-		program.add_argument("-n", "--name")
-			.help("要上传的文件名")
-			.action([](const std::string& value) { return value; })
-			.default_value(false);
-
-		program.add_argument("-k", "--key")
-			.help("要下载的文件键值")
-			.action([](const std::string& value) { return value; })
-			.default_value(false);
+		program.add_argument("-d", "--data")
+			.help("要上传/下载 的 文件名/文件键值")
+			.required()
+			.action([](const std::string& value) { return value; });
 	}
 
-	try
-	{
+	try {
 		program.parse_args(argc, argv);
 	}
-	catch (const std::runtime_error & err)
-	{
+	catch (const std::runtime_error & err) {
 		std::cout << err.what() << std::endl;
 		std::cout << program;
 		exit(0);
 	}
 
-	std::string key, filename;
-	std::string method = program.get<std::string>("--method");
-	if (method == "upload")
+	Method method = program.get<Method>("method");
+	std::string data = program.get<std::string>("--data");
+
+	dbg(method);
+
+	switch (method)
 	{
-		filename = program.get<std::string>("--name");
-		Upload(filename);
+	case Method::UPLOAD: {
+		Upload(data);
+		break;
 	}
-	else if (method == "download")
-	{
-		key = program.get<std::string>("--key");
-		Download(key);
+	case Method::DOWNLOAD: {
+		Download(data);
+		break;
 	}
-	else
-	{
-		exit(0);
+	case Method::OTHER: {
+		std::cout << program;
+		break;
+	}
 	}
 
 	return 0;
